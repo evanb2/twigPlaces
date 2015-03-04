a718646e19c3221c0e61ac3dd3cf6d6a3d3489fe
evan butler & virginie trubiano <student@epicodus.com>
2015-03-04 15:11:06 -0800

    create a bunch of other files

---
+++ views/create_places.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Congrats</title>
+</head>
+<body>
+    <h1>You added a new place !</h1>
+
+    <p>{{ newplace.getPlace }}</p>
+    <p><a href='/'>View your list of places</p>
+</body>
+</html>
---
+++ views/delete_places.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+        <title>Empty list</title>
+</head>
+<body>
+        <h1>You haven't been anywhere. (That's sad)</h1>
+        <p><a href='/'>Home</a></p>
+</body>
+</html>
